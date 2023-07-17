602478bcd765b6e914a0c146a300afde33bf006f
Luciano <lucho.2012.tandil@gmail.com>
2023-07-17 20:48:49 -0300

    Se agrega suma de menciones a beneficios

--- analiza_datos.py
+++ analiza_datos.py
@@ -5,6 +5,7 @@ total_registros = 0
 dedicacion = { "full_time": [], "part_time": [] }
 pago_dolares = { "si": [], "no": [], "parcial": []}
 conformidad = {}
+beneficios = {}
 todos = []
 
 with open("encuesta.csv", 'r') as csvfile:
@@ -13,6 +14,7 @@ with open("encuesta.csv", 'r') as csvfile:
     for data in csvreader:
         #Se genera listado completo
         todos.append(data)
+        print(data)
         print("")
         
         #Cataloga por conformidad
@@ -23,15 +25,25 @@ with open("encuesta.csv", 'r') as csvfile:
                 conformidad[str(data[13])] = []
                 conformidad[str(data[13])].append(data)
 
-        #Cataloga por dedicacion horaria
-        if (data[2] == 'Full-Time'):
-            dedicacion["full_time"].append(data)
-        if (data[2] == 'Part-Time'):
-            dedicacion["part_time"].append(data)
+            #Cataloga por dedicacion horaria
+            if (data[2] == 'Full-Time'):
+                dedicacion["full_time"].append(data)
+            if (data[2] == 'Part-Time'):
+                dedicacion["part_time"].append(data)
 
-        #Cataloga por pago en dolares
-        if (data[6] == ''):
-            pago_dolares["no"].append(data)
+            #Cataloga por pago en dolares
+            if (data[6] == ''):
+                pago_dolares["no"].append(data)
+
+            #Mencion a beneficios
+            if (data[15] != ''):
+                reg_beneficios = data[14].split(',')
+                for beneficio in reg_beneficios:
+                    if (beneficio in beneficios):
+                        beneficios[beneficio] = beneficios[beneficio] + 1
+                    else:
+                        beneficios[beneficio] = 1
+
         total_registros = total_registros + 1
 
 
@@ -41,3 +53,8 @@ print("Conformidad salario:")
 keys_conformidad = conformidad.keys()
 for key in keys_conformidad:
     print("- "+str(key)+': '+str( len(conformidad[key]) / (total_registros/100) ) + "%")
+
+print("Menciones Beneficios")
+key_beneficios = beneficios.keys()
+for key in key_beneficios:
+    print("- "+str(key)+': '+ str(beneficios[key]))
